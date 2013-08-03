1a84660466dc42f6518d365018cda2a60a0058a4
Max Kueng <me@maxkueng.com>
2013-08-03 16:34:16 +0200

    Added support for stream options

--- index.js
+++ index.js
@@ -4,12 +4,18 @@ var Transform = require('readable-stream').Transform,
 	inherits = require('util').inherits,
 	qry = require('qry');
 
-function QueryStream (query) {
+function QueryStream (query, options) {
 	if (!(this instanceof QueryStream)) {
-		return new QueryStream(query);
+		return new QueryStream(query, options);
 	}
 
-	Transform.call(this, { objectMode : true });
+	if (!(options instanceof Object)) {
+		options = {};
+	}
+
+	options.objectMode = true;
+
+	Transform.call(this, options);
 	this.match = qry(query);
 }
 
